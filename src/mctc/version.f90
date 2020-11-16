ac707d983af2b2051029ae32ef5c8eeb1fb967ac
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-11-16 09:23:13 +0100

    Allow log extension for xyz files
    - only write xyz in exponential notation for large numbers
    - bump version to 0.1.2
    - add manpage to mctc-convert

--- src/mctc/version.f90
+++ src/mctc/version.f90
@@ -21,10 +21,10 @@ module mctc_version
 
 
    !> String representation of the mctc-lib version
-   character(len=*), parameter :: mctc_version_string = "0.1.1"
+   character(len=*), parameter :: mctc_version_string = "0.1.2"
 
    !> Numeric representation of the mctc-lib version
-   integer, parameter :: mctc_version_compact(3) = [0, 1, 1]
+   integer, parameter :: mctc_version_compact(3) = [0, 1, 2]
 
 
 contains
