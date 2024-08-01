214a30afa2a407da8c254aae6c8b8bdd9e51dff9
awvwgk <28669218+awvwgk@users.noreply.github.com>
2024-08-01 08:55:08 +0000

    Deploying to gh-pages from  @ d3efa25c98b63c719d83031dd5ec83a7ddd24269 🚀

--- src/genformat.f90
+++ src/genformat.f90
@@ -12,79 +12,218 @@
 ! See the License for the specific language governing permissions and
 ! limitations under the License.
 
-module mctc_io_write_genformat
+module mctc_io_read_genformat
    use mctc_env_accuracy, only : wp
+   use mctc_env_error, only : error_type
    use mctc_io_constants, only : pi
-   use mctc_io_convert, only : autoaa
-   use mctc_io_math, only : matinv_3x3
-   use mctc_io_symbols, only : to_symbol
-   use mctc_io_structure, only : structure_type
+   use mctc_io_convert, only : aatoau
+   use mctc_io_structure, only : structure_type, new
+   use mctc_io_structure_info, only : structure_info
+   use mctc_io_symbols, only : to_number, symbol_length
+   use mctc_io_utils, only : next_line, token_type, next_token, io_error, filename, &
+      read_next_token, to_string
    implicit none
    private
 
-   public :: write_genformat
+   public :: read_genformat
 
 
 contains
 
 
-subroutine write_genformat(mol, unit)
-   class(structure_type), intent(in) :: mol
-   integer, intent(in) :: unit
-   integer :: iat, izp
-   real(wp), parameter :: zero3(3) = 0.0_wp
-   real(wp), allocatable :: inv_lat(:, :)
-   real(wp), allocatable :: abc(:, :)
-   logical :: helical
+subroutine read_genformat(mol, unit, error)
 
-   helical = .false.
-   write(unit, '(i0, 1x)', advance='no') mol%nat
-   if (.not.any(mol%periodic)) then
-      write(unit, '("C")') ! cluster
-   else
-      helical = count(mol%periodic) == 1 .and. mol%periodic(3) .and. size(mol%lattice, 2) == 1
-      if (helical) then
-         write(unit, '("H")') ! helical
-      else
-         if (mol%info%cartesian) then
-            write(unit, '("S")') ! supercell
-         else
-            write(unit, '("F")') ! fractional
-         endif
+   !> Instance of the molecular structure data
+   type(structure_type),intent(out) :: mol
+
+   !> File handle
+   integer,intent(in) :: unit
+
+   !> Error handling
+   type(error_type), allocatable, intent(out) :: error
+
+   character(len=:), allocatable :: line
+   integer :: natoms, nspecies, iatom, dummy, isp, ilat, stat, istart, iend
+   logical :: cartesian, periodic(3)
+   real(wp) :: coord(3), origin(3)
+   character(len=1) :: variant
+   type(token_type) :: token
+   character(len=symbol_length), allocatable :: species(:), sym(:)
+   real(wp), allocatable :: xyz(:, :), abc(:, :), lattice(:, :)
+   type(structure_info) :: info
+   integer :: pos, lnum
+
+   lnum = 0
+   call advance_line(unit, line, pos, lnum, stat)
+   call read_next_token(line, pos, token, natoms, stat)
+   if (stat /= 0 .or. natoms < 1) then
+      call io_error(error, "Could not read number of atoms", &
+         & line, token, filename(unit), lnum, "expected integer value")
+      return
+   end if
+
+   allocate(species(natoms))
+   allocate(sym(natoms))
+   allocate(xyz(3, natoms))
+   allocate(abc(3, natoms))
+
+   call next_token(line, pos, token)
+   select case(line(token%first:token%last))
+   case('c', 'C')
+      cartesian = .true.
+      periodic = .false.
+   case('s', 'S')
+      cartesian = .true.
+      periodic = .true.
+      allocate(lattice(3, 3), source=0.0_wp)
+   case('f', 'F')
+      cartesian = .false.
+      periodic = .true.
+      allocate(lattice(3, 3), source=0.0_wp)
+   case('h', 'H')
+      cartesian = .true.
+      periodic = [.false., .false., .true.]
+      allocate(lattice(3, 1), source=0.0_wp)
+   case default
+      call io_error(error, "Invalid input version found", &
+         & line, token, filename(unit), lnum, "unknown identifier")
+      return
+   end select
+
+   call advance_line(unit, line, pos, lnum, stat)
+   isp = 0
+   do while(pos < len(line))
+      call next_token(line, pos, token)
+      isp = isp + 1
+      token%last = min(token%last, token%first + symbol_length - 1)
+      species(isp) = line(token%first:token%last)
+      if (to_number(species(isp)) == 0) then
+         call io_error(error, "Cannot map symbol to atomic number", &
+            & line, token, filename(unit), lnum, "unknown element")
+         return
       end if
-   endif
+   end do
+   nspecies = isp
 
-   do izp = 1, mol%nid
-      write(unit, '(1x, a)', advance='no') trim(mol%sym(izp))
-   enddo
-   write(unit, '(a)')
-
-   if (.not.any(mol%periodic) .or. mol%info%cartesian) then
-      ! now write the cartesian coordinates
-      do iat = 1, mol%nat
-         write(unit, '(2i5, 3es24.14)') iat, mol%id(iat), mol%xyz(:, iat)*autoaa
-      enddo
-   else
-      inv_lat = matinv_3x3(mol%lattice)
-      abc = matmul(inv_lat, mol%xyz)
-      ! now write the fractional coordinates
-      do iat = 1, mol%nat
-         write(unit, '(2i5, 3es24.15)') iat, mol%id(iat), abc(:, iat)
-      enddo
-   endif
-
-   if (any(mol%periodic)) then
-      write(unit, '(3f20.14)') zero3
-      ! write the lattice parameters
-      if (helical) then
-         write(unit, '(2f20.14,1x,i0)') &
-            & mol%lattice(1, 1)*autoaa, mol%lattice(2, 1)*180.0_wp/pi, nint(mol%lattice(3, 1))
-      else
-         write(unit, '(3f20.14)') mol%lattice(:, :)*autoaa
+   do iatom = 1, natoms
+      token = token_type(0, 0)
+      call advance_line(unit, line, pos, lnum, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, dummy, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, isp, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(1), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(2), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(3), stat)
+      if (stat /= 0) then
+         call io_error(error, "Cannot read coordinates", &
+            & line, token, filename(unit), lnum, "unexpected value")
+         return
       end if
-   endif
+      sym(iatom) = species(isp)
+      if (cartesian) then
+         xyz(:, iatom) = coord * aatoau
+      else
+         abc(:, iatom) = coord
+      end if
+   end do
 
-end subroutine write_genformat
+   if (any(periodic)) then
+      call advance_line(unit, line, pos, lnum, stat)
+      if (stat /= 0) then
+         call io_error(error, "Unexpected end of file", &
+            & line, token_type(0, 0), filename(unit), lnum, "missing lattice information")
+         return
+      end if
+      if (stat == 0) &
+         call read_next_token(line, pos, token, origin(1), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, origin(2), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, origin(3), stat)
+      if (stat /= 0) then
+         call io_error(error, "Cannot read origin", &
+            & line, token, filename(unit), lnum, "expected real value")
+         return
+         end if
+   end if
+
+   if (all(periodic)) then
+      do ilat = 1, 3
+         call advance_line(unit, line, pos, lnum, stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, coord(1), stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, coord(2), stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, coord(3), stat)
+         if (stat /= 0) then
+            call io_error(error, "Cannot read lattice vector", &
+               & line, token, filename(unit), lnum, "expected real value")
+            return
+         end if
+         lattice(:, ilat) = coord * aatoau
+      end do
+      if (.not.cartesian) then
+         xyz = matmul(lattice, abc)
+      end if
+   end if
+
+   if (count(periodic) == 1) then
+      call advance_line(unit, line, pos, lnum, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(1), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(2), stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, coord(3), stat)
+      if (stat /= 0) then
+         call io_error(error, "Cannot read lattice vector", &
+            & line, token, filename(unit), lnum, "expected real value")
+         return
+      end if
+      if (coord(3) < 1) then
+         call io_error(error, "Invalid helical axis rotation order", &
+            & line, token, filename(unit), lnum, "expected positive value")
+         return
+      end if
+
+      ! Store helical axis in *first* lattice vector, however it is not an actual
+      ! lattice vector as on would expect but a screw axis
+      lattice(:, 1) = [coord(1) * aatoau, coord(2) * pi / 180.0_wp, coord(3)]
+   end if
+
+   if (any(periodic)) then
+      xyz(:, :) = xyz - spread(origin, 2, natoms)
+   end if
+
+   info = structure_info(cartesian=cartesian)
+   call new(mol, sym, xyz, lattice=lattice, periodic=periodic, info=info)
+
+contains
+
+subroutine advance_line(unit, line, pos, num, stat)
+   integer,intent(in) :: unit
+   integer, intent(out) :: pos
+   integer, intent(inout) :: num
+   character(len=:), allocatable, intent(out) :: line
+   integer, intent(out) :: stat
+   integer :: ihash
+
+   stat = 0
+   do while(stat == 0)
+      call next_line(unit, line, pos, num, stat)
+      ihash = index(line, '#')
+      if (ihash > 0) line = line(:ihash-1)
+      if (len_trim(line) > 0) exit
+   end do
+   line = trim(adjustl(line))
+end subroutine advance_line
+
+end subroutine read_genformat
 
 
-end module mctc_io_write_genformat
+end module mctc_io_read_genformat
--- src/qchem.f90
+++ src/qchem.f90
@@ -12,46 +12,158 @@
 ! See the License for the specific language governing permissions and
 ! limitations under the License.
 
-module mctc_io_write_qchem
+module mctc_io_read_qchem
    use mctc_env_accuracy, only : wp
-   use mctc_io_convert, only : autoaa
-   use mctc_io_structure, only : structure_type
+   use mctc_env_error, only : error_type
+   use mctc_io_convert, only : aatoau
+   use mctc_io_resize, only : resize
+   use mctc_io_symbols, only : symbol_length, to_number, to_symbol
+   use mctc_io_structure, only : structure_type, new
+   use mctc_io_utils, only : next_line, token_type, next_token, io_error, filename, &
+      read_next_token, read_token
    implicit none
    private
 
-   public :: write_qchem
+   public :: read_qchem
+
+   integer, parameter :: initial_size = 64
 
 contains
 
-subroutine write_qchem(self, unit)
+subroutine read_qchem(mol, unit, error)
 
    !> Instance of the molecular structure data
-   class(structure_type), intent(in) :: self
+   type(structure_type), intent(out) :: mol
 
    !> File handle
    integer, intent(in) :: unit
 
-   integer :: iat
-   logical :: expo
+   !> Error handling
+   type(error_type), allocatable, intent(out) :: error
 
-   write(unit, '(a)') "$molecule"
-   write(unit, '(*(1x, i0))') nint(self%charge), self%uhf + 1
+   integer :: stat, pos, lnum, izp, iat
+   integer :: charge, multiplicity
+   type(token_type) :: token
+   character(len=:), allocatable :: line
+   real(wp) :: x, y, z
+   character(len=symbol_length), allocatable :: sym(:)
+   real(wp), allocatable :: xyz(:, :), abc(:, :), lattice(:, :)
+   logical :: is_frac, periodic(3)
 
-   expo = maxval(self%xyz) > 1.0e+5 .or. minval(self%xyz) < -1.0e+5
-   if (expo) then
-      do iat = 1, self%nat
-         write(unit, '(a, 1x, 3es24.14)') &
-           self%sym(self%id(iat)), self%xyz(:, iat) * autoaa
-      end do
-   else
-      do iat = 1, self%nat
-         write(unit, '(a, 1x, 3f24.14)') &
-           self%sym(self%id(iat)), self%xyz(:, iat) * autoaa
-      end do
+   iat = 0
+   lnum = 0
+   stat = 0
+
+   do while(stat == 0)
+      call next_line(unit, line, pos, lnum, stat)
+      if (stat /= 0) exit
+
+      call next_token(line, pos, token)
+      if (token%first > len(line)) cycle
+      if (to_lower(line(token%first:token%last)) == '$molecule') exit
+   end do
+
+   if (stat /= 0) then
+      call io_error(error, "No atoms found", &
+         & line, token_type(0, 0), filename(unit), lnum+1, "expected molecule block")
+      return
    end if
 
-   write(unit, '(a)') "$end"
+   call next_line(unit, line, pos, lnum, stat)
+   if (stat == 0) &
+      call read_next_token(line, pos, token, charge, stat)
+   if (stat == 0) &
+      call read_next_token(line, pos, token, multiplicity, stat)
+   if (stat /= 0) then
+      call io_error(error, "Failed to read charge and multiplicity", &
+         & line, token, filename(unit), lnum, "expected integer value")
+      return
+   end if
 
-end subroutine write_qchem
-   
-end module mctc_io_write_qchem
+   allocate(sym(initial_size), source=repeat(' ', symbol_length))
+   allocate(xyz(3, initial_size), source=0.0_wp)
+
+   do while(stat == 0)
+      call next_line(unit, line, pos, lnum, stat)
+      if (stat /= 0) exit
+
+      call next_token(line, pos, token)
+      if (to_lower(line(token%first:token%last)) == '$end') exit
+
+      if (iat >= size(sym)) call resize(sym)
+      if (iat >= size(xyz, 2)) call resize(xyz)
+      iat = iat + 1
+
+      token%last = min(token%last, token%first + symbol_length - 1)
+      sym(iat) = line(token%first:token%last)
+      if (to_number(sym(iat)) == 0) then
+         call read_token(line, token, izp, stat)
+         sym(iat) = to_symbol(izp)
+      end if
+      if (stat /= 0) then
+         call io_error(error, "Cannot map symbol to atomic number", &
+            & line, token, filename(unit), lnum, "unknown element")
+         return
+      end if
+
+      call read_next_token(line, pos, token, x, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, y, stat)
+      if (stat == 0) &
+         call read_next_token(line, pos, token, z, stat)
+      if (stat /= 0) then
+         call io_error(error, "Cannot read coordinates", &
+            & line, token, filename(unit), lnum, "expected real value")
+         return
+      end if
+
+      xyz(:, iat) = [x, y, z] * aatoau
+   end do
+
+   if (stat /= 0) then
+      call io_error(error, "Failed to read molecule block", &
+         & line, token_type(0, 0), filename(unit), lnum, "unexpected end of input")
+      return
+   end if
+
+   call new(mol, sym(:iat), xyz, charge=real(charge, wp), uhf=multiplicity-1)
+end subroutine read_qchem
+
+!> Convert input string to lowercase
+elemental function to_lower(str) result(lcstr)
+
+   !> Input string
+   character(len=*), intent(in) :: str
+
+   !> Lowercase version of string
+   character(len=len(str)):: lcstr
+
+   integer :: ilen, iquote, i, iav, iqc
+   integer, parameter :: offset = iachar('A') - iachar('a')
+
+   ilen = len(str)
+   iquote = 0
+   lcstr = str
+
+   do i = 1, ilen
+      iav = iachar(str(i:i))
+      if (iquote == 0 .and. (iav == 34 .or.iav == 39)) then
+         iquote = 1
+         iqc = iav
+         cycle
+      end if
+      if (iquote == 1 .and. iav==iqc) then
+         iquote=0
+         cycle
+      end if
+      if (iquote == 1) cycle
+      if (iav >= iachar('A') .and. iav <= iachar('Z')) then
+         lcstr(i:i) = achar(iav - offset)
+      else
+         lcstr(i:i) = str(i:i)
+      end if
+   end do
+
+end function to_lower
+
+end module mctc_io_read_qchem
