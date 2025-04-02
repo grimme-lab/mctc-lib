eeae20e8f9b7d8b9e2d7d9488339caebc8b775cc
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2025-04-02 20:45:27 +0000

    Deploying to gh-pages from  @ e741287aa99df5e06738843f3e2af1dd68d06070 🚀

--- src/aims.f90
+++ src/aims.f90
@@ -12,51 +12,145 @@
 ! See the License for the specific language governing permissions and
 ! limitations under the License.
 
-module mctc_io_write_aims
+module mctc_io_read_aims
    use mctc_env_accuracy, only : wp
-   use mctc_io_convert, only : autoaa
-   use mctc_io_structure, only : structure_type
+   use mctc_env_error, only : error_type, fatal_error
+   use mctc_io_convert, only : aatoau
+   use mctc_io_resize, only : resize
+   use mctc_io_symbols, only : symbol_length, to_number
+   use mctc_io_structure, only : structure_type, new
+   use mctc_io_utils, only : next_line, token_type, next_token, io_error, filename, &
+      read_next_token, to_string
    implicit none
    private
 
-   public :: write_aims
+   public :: read_aims
+
+   integer, parameter :: initial_size = 64
 
 contains
 
-subroutine write_aims(self, unit)
+subroutine read_aims(mol, unit, error)
 
    !> Instance of the molecular structure data
-   class(structure_type), intent(in) :: self
+   type(structure_type), intent(out) :: mol
 
    !> File handle
    integer, intent(in) :: unit
 
-   integer :: iat, ilt
-   logical :: expo
+   !> Error handling
+   type(error_type), allocatable, intent(out) :: error
 
-   expo = maxval(self%xyz) > 1.0e+5 .or. minval(self%xyz) < -1.0e+5
-   if (expo) then
-      do iat = 1, self%nat
-         write(unit, '(a, 1x, 3es24.14, 1x, a)') &
-            "atom", self%xyz(:, iat) * autoaa, trim(self%sym(self%id(iat)))
-      end do
-   else
-      do iat = 1, self%nat
-         write(unit, '(a, 1x, 3f24.14, 1x, a)') &
-            "atom", self%xyz(:, iat) * autoaa, trim(self%sym(self%id(iat)))
-      end do
-   end if
+   integer :: stat, pos, lnum, ilt, iat
+   type(token_type) :: token
+   character(len=:), allocatable :: line
+   real(wp) :: x, y, z
+   character(len=symbol_length), allocatable :: sym(:)
+   real(wp), allocatable :: xyz(:, :), abc(:, :), lattice(:, :)
+   logical :: is_frac, periodic(3)
+   logical, allocatable :: frac(:)
 
-   if (any(self%periodic)) then
-      if (size(self%lattice, 2) /= 3) return
-      do ilt = 1, 3
-         if (self%periodic(ilt)) then
-            write(unit, '(a, 1x, 3f24.14)') &
-               "lattice_vector", self%lattice(:, ilt) * autoaa
+   allocate(sym(initial_size), source=repeat(' ', symbol_length))
+   allocate(xyz(3, initial_size), source=0.0_wp)
+   allocate(abc(3, initial_size), source=0.0_wp)
+   allocate(frac(initial_size), source=.false.)
+
+   iat = 0
+   ilt = 0
+   periodic(:) = .false.
+
+   lnum = 0
+   stat = 0
+   do while(stat == 0)
+      call next_line(unit, line, pos, lnum, stat)
+      if (stat /= 0) exit
+      if (len(line) == 0) cycle
+      if (line(1:1) == "#") cycle
+
+      call next_token(line, pos, token)
+      select case(line(token%first:token%last))
+      case("atom", "atom_frac")
+         is_frac = token%last - token%first + 1 > 4
+         call read_next_token(line, pos, token, x, stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, y, stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, z, stat)
+         if (stat == 0) &
+            call next_token(line, pos, token)
+         if (stat /= 0) then
+            call io_error(error, "Cannot read coordinates", &
+               & line, token, filename(unit), lnum, "expected real value")
+            exit
          end if
-      end do
+
+         if (iat >= size(sym)) call resize(sym)
+         if (iat >= size(xyz, 2)) call resize(xyz)
+         if (iat >= size(abc, 2)) call resize(abc)
+         if (iat >= size(frac)) call resize(frac)
+         iat = iat + 1
+
+         token%last = min(token%last, token%first + symbol_length - 1)
+         sym(iat) = line(token%first:token%last)
+         if (to_number(sym(iat)) == 0) then
+            call io_error(error, "Cannot map symbol to atomic number", &
+               & line, token, filename(unit), lnum, "unknown element")
+            exit
+         end if
+         frac(iat) = is_frac
+         if (frac(iat)) then
+            abc(:, iat) = [x, y, z]
+            xyz(:, iat) = 0.0_wp
+         else
+            abc(:, iat) = 0.0_wp
+            xyz(:, iat) = [x, y, z] * aatoau
+         end if
+
+      case("lattice_vector")
+         ilt = ilt + 1
+         if (ilt > 3) then
+            call io_error(error, "Too many lattice vectors", &
+               & line, token, filename(unit), lnum, "forth lattice vector found")
+            exit
+         end if
+         call read_next_token(line, pos, token, x, stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, y, stat)
+         if (stat == 0) &
+            call read_next_token(line, pos, token, z, stat)
+         if (stat /= 0) then
+            call io_error(error, "Cannot read lattice vectors", &
+               & line, token, filename(unit), lnum, "expected real value")
+            exit
+         end if
+
+         if (.not.allocated(lattice)) allocate(lattice(3, 3), source=0.0_wp)
+         lattice(:, ilt) = [x, y, z] * aatoau
+
+      case default
+         call io_error(error, "Unexpected keyword found", &
+            & line, token, filename(unit), lnum, "invalid in this context")
+         exit
+      end select
+
+   end do
+   if (allocated(error)) return
+
+   if (iat == 0) then
+      token = token_type(0, 0)
+      call io_error(error, "No atoms found", &
+         & line, token, filename(unit), lnum+1, "expected atom specification")
+      return
    end if
 
-end subroutine write_aims
-   
-end module mctc_io_write_aims
+   if (allocated(lattice)) then
+      xyz(ilt+1:3, :iat) = xyz(ilt+1:3, :iat) + abc(ilt+1:3, :iat) * aatoau
+      xyz(:ilt, :iat) = xyz(:ilt, :iat) + matmul(lattice(:ilt, :ilt), abc(:ilt, :iat))
+      periodic(:ilt) = .true.
+   end if
+
+   call new(mol, sym(:iat), xyz, lattice=lattice, periodic=periodic)
+
+end subroutine read_aims
+
+end module mctc_io_read_aims
